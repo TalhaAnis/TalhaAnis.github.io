22e1718e11967f52036be25f9d5415833b6b6849
Talha Anis <t.anis60.ta@gmail.com>
2021-05-27 05:38:07 +0500

    Updates

---
+++ precache-manifest.ee1355f9edaf4945d0125174ac412fdb.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eca90482e59f289b50d3cff0a8d8a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d10f4beb6804de7bce",
+    "url": "/static/css/2.dd8ad4bb.chunk.css"
+  },
+  {
+    "revision": "370988877d7eee359fd6",
+    "url": "/static/css/main.d6171c45.chunk.css"
+  },
+  {
+    "revision": "c8d10f4beb6804de7bce",
+    "url": "/static/js/2.1e311997.chunk.js"
+  },
+  {
+    "revision": "20340ba8cd52f61a60aa2ead21351ecb",
+    "url": "/static/js/2.1e311997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370988877d7eee359fd6",
+    "url": "/static/js/main.4b87abe5.chunk.js"
+  },
+  {
+    "revision": "23fc3f2e02988ff9210c",
+    "url": "/static/js/runtime-main.69056d96.js"
+  },
+  {
+    "revision": "e2e312083bc8806dd6f9be0b126802ce",
+    "url": "/static/media/arrow.e2e31208.svg"
+  },
+  {
+    "revision": "9a8d4c79a81d79fc5823e008431b1e72",
+    "url": "/static/media/aws.9a8d4c79.svg"
+  },
+  {
+    "revision": "2f571c9e30fdb398082329aa14301174",
+    "url": "/static/media/bootstrap.2f571c9e.svg"
+  },
+  {
+    "revision": "17b662141abe2bf02879fc8262b42d27",
+    "url": "/static/media/cloudflare.17b66214.svg"
+  },
+  {
+    "revision": "e67644d26d8072c6ac28a0f31c3db902",
+    "url": "/static/media/devops.e67644d2.svg"
+  },
+  {
+    "revision": "d3430b8b8d32c1068d31da4e35b975aa",
+    "url": "/static/media/docker.d3430b8b.svg"
+  },
+  {
+    "revision": "14e869f76e5e47c6a7f2f7560be736bc",
+    "url": "/static/media/golang.14e869f7.svg"
+  },
+  {
+    "revision": "9ea3047330a1a2e8451bb1c485f9b21f",
+    "url": "/static/media/html5.9ea30473.svg"
+  },
+  {
+    "revision": "f6d0312a0ee9eb54634a026d7b4ab062",
+    "url": "/static/media/javascript.f6d0312a.svg"
+  },
+  {
+    "revision": "858fbaaa1ee892672bc29f92405a0d66",
+    "url": "/static/media/mobile-close.858fbaaa.svg"
+  },
+  {
+    "revision": "8608ac6315fa5c0e7fedafd58ca0f1b2",
+    "url": "/static/media/mobile-menu.8608ac63.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/static/media/mongodb.d571053a.svg"
+  },
+  {
+    "revision": "b04e488e156e32b474e695c43c9e7ac4",
+    "url": "/static/media/mysql.b04e488e.svg"
+  },
+  {
+    "revision": "30808c3e619625dc616df73b3b581730",
+    "url": "/static/media/nginx.30808c3e.svg"
+  },
+  {
+    "revision": "0b4524567b430e17d5d0d9e44e22332c",
+    "url": "/static/media/puppet.0b452456.svg"
+  },
+  {
+    "revision": "53bff15dd5a36f93eb6504434610eea4",
+    "url": "/static/media/python-3.53bff15d.svg"
+  },
+  {
+    "revision": "f6672494dd401b743d0a54200e0e8437",
+    "url": "/static/media/talha-anis.f6672494.png"
+  }
+]);
