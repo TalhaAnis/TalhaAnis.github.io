06e4fc3f616e348b42fab322051a4e95b791ad12
Talha Anis <t.anis60.ta@gmail.com>
2021-05-27 05:30:17 +0500

    Added portfolio

---
+++ precache-manifest.5f474d1421951a19a2a0f83182c75451.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea5de3e37980944a4ea44e040a45103",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "d6fef90aae91badbc8b3",
+    "url": "/about-me/static/css/2.dd8ad4bb.chunk.css"
+  },
+  {
+    "revision": "7efc36a6e072a9e342ef",
+    "url": "/about-me/static/css/main.d6171c45.chunk.css"
+  },
+  {
+    "revision": "d6fef90aae91badbc8b3",
+    "url": "/about-me/static/js/2.b67dd8b3.chunk.js"
+  },
+  {
+    "revision": "20340ba8cd52f61a60aa2ead21351ecb",
+    "url": "/about-me/static/js/2.b67dd8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7efc36a6e072a9e342ef",
+    "url": "/about-me/static/js/main.9ed46e5f.chunk.js"
+  },
+  {
+    "revision": "e52cc062d187456a52b0",
+    "url": "/about-me/static/js/runtime-main.825de4e3.js"
+  },
+  {
+    "revision": "e2e312083bc8806dd6f9be0b126802ce",
+    "url": "/about-me/static/media/arrow.e2e31208.svg"
+  },
+  {
+    "revision": "9a8d4c79a81d79fc5823e008431b1e72",
+    "url": "/about-me/static/media/aws.9a8d4c79.svg"
+  },
+  {
+    "revision": "2f571c9e30fdb398082329aa14301174",
+    "url": "/about-me/static/media/bootstrap.2f571c9e.svg"
+  },
+  {
+    "revision": "17b662141abe2bf02879fc8262b42d27",
+    "url": "/about-me/static/media/cloudflare.17b66214.svg"
+  },
+  {
+    "revision": "e67644d26d8072c6ac28a0f31c3db902",
+    "url": "/about-me/static/media/devops.e67644d2.svg"
+  },
+  {
+    "revision": "d3430b8b8d32c1068d31da4e35b975aa",
+    "url": "/about-me/static/media/docker.d3430b8b.svg"
+  },
+  {
+    "revision": "14e869f76e5e47c6a7f2f7560be736bc",
+    "url": "/about-me/static/media/golang.14e869f7.svg"
+  },
+  {
+    "revision": "9ea3047330a1a2e8451bb1c485f9b21f",
+    "url": "/about-me/static/media/html5.9ea30473.svg"
+  },
+  {
+    "revision": "f6d0312a0ee9eb54634a026d7b4ab062",
+    "url": "/about-me/static/media/javascript.f6d0312a.svg"
+  },
+  {
+    "revision": "858fbaaa1ee892672bc29f92405a0d66",
+    "url": "/about-me/static/media/mobile-close.858fbaaa.svg"
+  },
+  {
+    "revision": "8608ac6315fa5c0e7fedafd58ca0f1b2",
+    "url": "/about-me/static/media/mobile-menu.8608ac63.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/about-me/static/media/mongodb.d571053a.svg"
+  },
+  {
+    "revision": "b04e488e156e32b474e695c43c9e7ac4",
+    "url": "/about-me/static/media/mysql.b04e488e.svg"
+  },
+  {
+    "revision": "30808c3e619625dc616df73b3b581730",
+    "url": "/about-me/static/media/nginx.30808c3e.svg"
+  },
+  {
+    "revision": "0b4524567b430e17d5d0d9e44e22332c",
+    "url": "/about-me/static/media/puppet.0b452456.svg"
+  },
+  {
+    "revision": "53bff15dd5a36f93eb6504434610eea4",
+    "url": "/about-me/static/media/python-3.53bff15d.svg"
+  },
+  {
+    "revision": "f6672494dd401b743d0a54200e0e8437",
+    "url": "/about-me/static/media/talha-anis.f6672494.png"
+  }
+]);
